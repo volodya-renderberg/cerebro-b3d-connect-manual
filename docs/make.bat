7fac73227fb0cb2d09f556884fcdeef5b9dd61be
vpylev <vpylev@magicfactory.ru>
2023-10-11 21:04:36 +0300

    add config

--- docs/make.bat
+++ docs/make.bat
@@ -5,12 +5,10 @@ pushd %~dp0
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+	set SPHINXBUILD=C:\Users\vpylev\AppData\Local\Programs\Python\Python311\python -m sphinx.cmd.build
 )
 set SOURCEDIR=source
 set BUILDDIR=build
-
-if "%1" == "" goto help
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -21,9 +19,11 @@ if errorlevel 9009 (
 	echo.may add the Sphinx directory to PATH.
 	echo.
 	echo.If you don't have Sphinx installed, grab it from
-	echo.http://sphinx-doc.org/
+	echo.https://www.sphinx-doc.org/
 	exit /b 1
 )
+
+if "%1" == "" goto help
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
