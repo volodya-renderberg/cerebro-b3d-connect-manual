50151df9917858251ee4555aded9b84efd3247a5
vpylev <vpylev@magicfactory.ru>
2023-10-12 14:25:12 +0300

    **

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,7 +65,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 RTD_NEW_THEME = True
 
